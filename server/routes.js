0bc180d1fe475909de2d04750a8eb0cf09b7986d
Inseok Seo <inseok.ucla@gmail.com>
2017-06-17 09:25:19 -0700

    comment out incomplete routes, so the server runs

--- server/routes.js
+++ server/routes.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 const controller = require('./controllers/controllers');
 
 // do we need these or will auth0 handle these for us?
-router.get('/login', controller.getLogin); // do we need this?
-router.get('/signup', controller.getSignUp); // do we need this?
-router.post('/login', controller.logIn); // do we need this?
-router.post('/signup', controller.signUp); // do we need this?
+// router.get('/login', controller.getLogin); // do we need this?
+// router.get('/signup', controller.getSignUp); // do we need this?
+// router.post('/login', controller.logIn); // do we need this?
+// router.post('/signup', controller.signUp); // do we need this?
 
 router.get('/questions', controller.fetchQuestions);
 router.get('/questions/:id', controller.fetchQuestionAndAnswers);
