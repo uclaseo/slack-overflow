ceb9f5e1eb65928d468edfcc429a095d83d616dd
Inseok Seo <inseok.ucla@gmail.com>
2017-06-18 12:40:01 -0700

    complete refactor back-end

--- server/routes.js
+++ server/routes.js
@@ -1,11 +1,24 @@
 const router = require('express').Router();
 const controller = require('./controllers/controllers');
+const jwt = require('express-jwt');
 
 // // do we need these or will auth0 handle these for us?
 // router.get('/login', controller.getLogin); // do we need this?
 // router.get('/signup', controller.getSignUp); // do we need this?
 // router.post('/login', controller.logIn); // do we need this?
 // router.post('/signup', controller.signUp); // do we need this?
+
+
+const authCheck = jwt({
+  secret: new Buffer('czR3QSYYTY6dImCfroTZqdXvpYOOwPsOJtVISE3kWyR1Q0AiEz4rVMSw_RvU5iL3'),
+  audience: 'ku4AUn23UfSipuIY4l8e8WovJ10X5XuY'
+});
+router.get('/api/public', function(req, res) {
+  res.json({message: 'hello from public endpoint, you dont need to be authenticated'})
+});
+router.get('/api/private', authCheck, function(req, res) {
+  res.json({message: 'hello from private endpoint, you are authenticated'})
+});
 
 router.get('/questions', controller.fetchQuestions);
 router.get('/questions/:id', controller.fetchQuestionAndAnswers);
