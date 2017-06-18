6b575e253773014f64c0f350218539be95533f89
Inseok Seo <inseok.ucla@gmail.com>
2017-06-18 09:14:21 -0700

    refresh does not change authentication state

--- public/app.js
+++ public/app.js
@@ -46,3 +46,18 @@ angular
       })
     $httpProvider.interceptors.push('jwtInterceptor');
   })
+  .run(function($rootScope, auth, store, jwtHelper, $location) {
+
+    $rootScope.$on('$locationChangeStart', function() {
+      var token = store.get('id_token');
+      if (token) {
+        if (!jwtHelper.isTokenExpired(token)) {
+          if (!auth.isAuthenticated) {
+            auth.authenticate(store.get('profile'), token);
+          }
+        }
+      } else {
+        $location.path('/home');
+      }
+    })
+  });
