7fe899814f0acae7257d179872157be7e8c5057d
Inseok Seo <inseok.ucla@gmail.com>
2017-06-18 09:45:49 -0700

    redirects to login page when token expires

--- public/app.js
+++ public/app.js
@@ -43,7 +43,28 @@ angular
         url: '/profile',
         templateUrl: './public/components/templates/profile.html',
         controller: 'profileController as user'
-      })
+      });
+
+
+    function redirect($q, $injector, $timeout, store, $location) {
+      var auth;
+      $timeout(function() {
+        auth = $injector.get('auth');
+      });
+      return {
+        responseError: function(rejection) {
+          if (rejection.status === 401) {
+            auth.signout();
+            store.remove('profile');
+            store.remove('id_token');
+            $location.path('/home');
+          }
+          return $q.reject(rejection);
+        }
+      }
+    }
+    $provide.factory('redirect', redirect);
+    $httpProvider.interceptors.push('redirect');
     $httpProvider.interceptors.push('jwtInterceptor');
   })
   .run(function($rootScope, auth, store, jwtHelper, $location) {
