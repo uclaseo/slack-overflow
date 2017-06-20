7f7c399d59c785012e353b10b0e87a27d98cdf13
Inseok Seo <inseok.ucla@gmail.com>
2017-06-20 12:32:28 -0700

    add field to array and renders automatically

--- public/components/controllers/profile.js
+++ public/components/controllers/profile.js
@@ -4,7 +4,6 @@
     .module('slackOverflowApp')
     .controller('profileController', ['$http', 'store', 'userService', function($http, store, userService) {
       const vm = this;
-      vm.getSecretMessage = getSecretMessage;
       vm.getFields = getFields;
       vm.addField = addField;
 
@@ -12,23 +11,17 @@
       vm.fields;
       vm.profile = store.get('profile');
 
-      function getFields(field) {
+      function getFields() {
         console.log('vm.profile in getFields in profile.js', vm.profile);
         vm.fields = vm.profile.userInfo.fields;
-        vm.fields.push(field);
         console.log('this is fields of a user', vm.fields);
         console.log('vm.profile after adding', vm.profile);
       };
       function addField(data) {
         console.log('profile FROM ADDFIELD', vm.profile);
         console.log('data', data);
-        vm.profile.userInfo.fields.push(field);
-      };
-      function getSecretMessage() {
-        $http.get('http://localhost:3456/api/private')
-        .then(function(response) {
-          vm.message = response.data.message;
-        })
+        vm.profile.userInfo.fields.push(data);
+        vm.getFields();
       };
       
     }]);
