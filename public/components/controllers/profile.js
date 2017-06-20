7fd2ab8ddd2e4ae5c1f200c45d24c6acb0c9f206
Inseok Seo <inseok.ucla@gmail.com>
2017-06-20 10:52:21 -0700

    does not correctly render fields, probably due to scope

--- public/components/controllers/profile.js
+++ public/components/controllers/profile.js
@@ -8,18 +8,24 @@
       vm.getFields = getFields;
       vm.addField = addField;
       vm.message;
+      vm.fields;
       vm.profile = store.get('profile');
-
+      vm.fields = ['java', 'python']
       function getFields() {
-        userService.getUserInfo(vm.profile)
-        .then((response) => {
-          console.log('response is this', response);
-          vm.profile = store.get('profile');
-          console.log(vm.profile);
-        }) 
-        .catch((error) => {
-          console.log('userService.getUserInfo fail', error);
-        });
+        // userService.getUserInfo(vm.profile)
+        // .then((response) => {
+        //   console.log('response is this', response);
+        //   vm.profile = store.get('profile');
+        //   console.log(vm.profile);
+        // }) 
+        // .catch((error) => {
+        //   console.log('userService.getUserInfo fail', error);
+        // });
+        console.log('vm.profile in getFields in profile.js', vm.profile);
+        vm.fields = vm.profile.userInfo.fields;
+        console.log('this is fields of a user', vm.fields);
+        vm.fields.push('javascript');
+        vm.fields.push('c++');
       };
       function addField() {
         console.log('userINFO FROM ADDFIELD', vm.profile.userInfo.fields);
