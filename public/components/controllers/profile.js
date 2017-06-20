8d03fc8ebfce7578d56c7fd059025b6f4e7a11ea
Inseok Seo <inseok.ucla@gmail.com>
2017-06-20 13:08:10 -0700

    click profile will render fields

--- public/components/controllers/profile.js
+++ public/components/controllers/profile.js
@@ -4,31 +4,41 @@
     .module('slackOverflowApp')
     .controller('profileController', ['$http', 'store', 'userService', function($http, store, userService) {
       const vm = this;
+      vm.profile = store.get('profile');
       vm.getFields = getFields;
       vm.addField = addField;
-
+      vm.removeField = removeField;
+      
       vm.message;
-      vm.fields;
-      vm.profile = store.get('profile');
+      vm.fields = vm.profile.userInfo.fields
 
       function getFields() {
-        console.log('vm.profile in getFields in profile.js', vm.profile);
+        // console.log('vm.profile in getFields in profile.js', vm.profile);
+        // vm.fields = vm.profile.userInfo.fields;
+        // console.log('this is fields of a user', vm.fields);
+        // console.log('vm.profile after adding', vm.profile);
+        vm.profile = store.get('profile');
+        console.log(vm.profile);
         vm.fields = vm.profile.userInfo.fields;
-        console.log('this is fields of a user', vm.fields);
-        console.log('vm.profile after adding', vm.profile);
       };
       function addField(data) {
-        console.log('profile FROM ADDFIELD', vm.profile);
-        console.log('data', data);
         let isUnique = true;
         for (let i = 0; i < vm.profile.userInfo.fields.length; i++) {
           if (vm.profile.userInfo.fields[i] === data) {
             isUnique = false;
           }
         }
-        isUnique ? vm.profile.userInfo.fields.push(data) : console.log('not unique');
+        if (isUnique) {
+          vm.profile.userInfo.fields.push(data);
+        }
+        store.set('profile', vm.profile);
         vm.getFields();
       };
+
+      function removeField() {
+        console.log('remove field');
+
+      }
       
     }]);
 })();
