299658360a0f96b8772620d1833456b68a22366a
Joshua Bahr <joshuabahr73@gmail.com>
2017-06-21 15:47:21 -0700

    add question entry and answers model and view

--- public/components/controllers/questionsAnsweredList.js
+++ public/components/controllers/questionsAnsweredList.js
@@ -1,8 +1,35 @@
 (function() {
   angular
     .module('slackOverflowApp')
-    .component('questionsAnsweredList', {
-      bindings: { questions: '<'},
-      templateUrl: '/public/components/templates/questionsAnsweredList.html'
-    })
+    .controller('questionsAnsweredListCtrl', ['QuestionsService', 'store', function(QuestionsService, store) {
+      var vm = this;
+      vm.questionsList = [];
+      vm.userId = store.get('profile').userInfo.Id;
+      vm.currentUsername = store.get('profile').userInfo.name;
+      vm.fieldArray = store.get('profile').userInfo.fields;
+
+      QuestionsService.getQuestionsForUser()
+        .then((resp) => {
+          answersObj = resp.data;
+          console.log('clicked', answersObj);
+        })
+        .then(() => {
+          var name = answersObj.results[0].name;
+          for (var i = 0; i < answersObj.results[0].questions.length; i++) {
+            var question = {};
+            question.name = name;
+            question.id = answersObj.results[0].questions[i].id;
+            question.title = answersObj.results[0].questions[i].title;
+            question.text = answersObj.results[0].questions[i].text;
+            question.field = answersObj.results[0].questions[i].field.name;
+            vm.questionsList.push(question);
+          }
+          // var sortedOutput = _.sortBy(output, 'id');
+          console.log(vm.questionsList);
+        })
+        .catch((err) => {
+          console.error('error fetching questions for user ', err);
+        })
+
+    }])
 })();
--- public/components/controllers/questionsAskedList.js
+++ public/components/controllers/questionsAskedList.js
@@ -1,8 +1,44 @@
 (function() {
   angular
     .module('slackOverflowApp')
-    .component('questionsAskedList', {
-      bindings: { questions: '<'},
-      templateUrl: '/public/components/templates/questionsAskedList.html'
-    })
-})();
+    .controller('questionsAskedListCtrl', ['QuestionsService', 'store', function(QuestionsService, store) {
+      var vm = this;
+      vm.questionsList = [];
+      vm.userId = store.get('profile').userInfo.Id;
+      vm.currentUsername = store.get('profile').userInfo.name;
+      vm.fieldArray = store.get('profile').userInfo.fields;
+      vm.questionsObj;
+
+      QuestionsService.getAllQuestions()
+        .then(function(resp) {
+          vm.questionsObj = resp.data;
+        })
+        .then(() => {
+          console.log('vm.fieldArray ', vm.fieldArray);
+          vm.questionsList = [];
+          for (var i = 0; i < vm.questionsObj.results.length; i++) {
+            var questionUsername;
+            questionUsername = vm.questionsObj.results[i].name;
+            for (var j = 0; j < vm.questionsObj.results[i].questions.length; j++) {
+              if (vm.questionsObj.results[i].questions[j].status === true && questionUsername !== vm.currentUsername) {
+                if (vm.fieldArray.indexOf(vm.questionsObj.results[i].questions[j].field.name ) !== -1) {
+                  var output = {};
+                  output.name = questionUsername;
+                  output.questionId = vm.questionsObj.results[i].questions[j].id;
+                  output.title = vm.questionsObj.results[i].questions[j].title;
+                  output.text = vm.questionsObj.results[i].questions[j].text;
+                  output.field = vm.questionsObj.results[i].questions[j].field.name;
+                  vm.questionsList.push(output);
+                }
+              }
+            }
+          }
+          // var sortedOutput = _.sortBy(questionsList, 'questionId');  
+          console.log('is this running ', vm.questionsList);
+        })
+        .catch((err) => {
+          console.error('error fetching questions ', err);
+        })
+    
+  }])
+})()
