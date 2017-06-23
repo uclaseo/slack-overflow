d291cccef64e6f5aa643859daabf17415053fac5
Inseok Seo <inseok.ucla@gmail.com>
2017-06-23 10:57:14 -0700

    set up server for chat

--- server/server.js
+++ server/server.js
@@ -10,6 +10,8 @@ const init = require('./init');
 const port = 3456;
 
 const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 app.use(bodyParser.json());
 app.use(morgan('dev'));
@@ -18,8 +20,16 @@ app.use('/', router);
 
 app.use(express.static(path.join(__dirname, '../')));
 
+
+io.on('connection', function(socket) {
+  socket.on('new-message', function(data) {
+    console.log('THIS IS DATAAAAAA');
+  });
+  socket.emit('update-message', data);
+});
+
 init()
   .then(() => {
-    app.listen(port, () => console.log(`app is listening on port ${port}`));
+    server.listen(port, () => console.log(`app is listening on port ${port}`));
   })
   .catch(err => console.error('unable to connect to database ', err));
