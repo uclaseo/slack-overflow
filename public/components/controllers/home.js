40a420a6cc413353afd9a3bc649ca9d8d5cc347f
Joshua Bahr <joshuabahr73@gmail.com>
2017-06-24 18:27:02 -0700

    add home page and other and change toolbar labels

---
+++ public/components/controllers/home.js
@@ -0,0 +1,14 @@
+(function() {
+  'use strict';
+  angular
+    .module('slackOverflowApp')
+    .controller('homeController', ['store', '$timeout', function(store, $timeout) {
+      var vm = this;
+
+      $timeout(function() {
+        vm.username = store.get('profile').userInfo.name;
+        vm.reputation = store.get('profile').userInfo.reputation;
+      }, 100);
+      
+    }]);
+})();
